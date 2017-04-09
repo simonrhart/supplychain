44df55c28154fbffa6ed0228a528fd7cb5df67ea
Simon Hart <simon@simonrhart.com>
2017-04-09 09:51:54 +0100

    more discovery service work

--- src/Microsoft.SupplyChain.Cloud.Registration.DiscoveryService/Controllers/DiscoveryController.cs
+++ src/Microsoft.SupplyChain.Cloud.Registration.DiscoveryService/Controllers/DiscoveryController.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Fabric.Description;
 using System.IO;
+using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
 using Microsoft.Azure.Devices;
+using Microsoft.Azure.Devices.Common.Security;
 using Microsoft.SupplyChain.Cloud.Registration.Contracts;
 using Microsoft.SupplyChain.Framework;
 using Newtonsoft.Json;
@@ -29,17 +32,35 @@ namespace Microsoft.SupplyChain.Cloud.Registration.DiscoveryService.Controllers
             _iotHubSection = configurationPackage.Settings.Sections["IoTHub"].Parameters;
         }
 
-        public async Task<string> Get(string id, string macAddress, int tokenExpirary = 0)
+        public async Task<string> Get(string id, string macAddress, bool signUsingPrimaryKey = true, int tokenExpiryInHours = 0)
         {
             var iotHubConnectionString = _iotHubSection["IoTHubConnectionString"].Value;
             if (string.IsNullOrEmpty(iotHubConnectionString))
                 throw new Exception("IoTHubConnectionString is not defined. Check ApplicationParameters.");
 
             var registryManager = RegistryManager.CreateFromConnectionString(iotHubConnectionString);
+
+            var device = await registryManager.GetDeviceAsync(id);
+            if (device == null)
+            {
+                throw new HttpResponseException(new HttpResponseMessage(HttpStatusCode.BadRequest)
+                {
+                    Content = new StringContent($"Unable to get device Id: {id} from the identity store."),
+                    ReasonPhrase = "Critical Exception"
+                });
+
+                
+            }
             var deviceTwin = await registryManager.GetTwinAsync(id);
-           
+
             if (deviceTwin == null)
-                throw new Exception("Unable to get DeviceTwin.");
+            {
+                throw new HttpResponseException(new HttpResponseMessage(HttpStatusCode.NotFound)
+                {
+                    Content = new StringContent($"Unable to get device twin for device Id: {id} from the identity store."),
+                    ReasonPhrase = "Critical Exception"
+                });
+            }
 
             // get the tags for this device.
             string tags = deviceTwin.Tags.ToJson();
@@ -52,17 +73,56 @@ namespace Microsoft.SupplyChain.Cloud.Registration.DiscoveryService.Controllers
                     deviceTags = js.Deserialize<DeviceTwinTags>(jr);
                 }
             }
-       
+
             if (deviceTags == null)
-                throw new Exception($"Failed to deseralized device twin tags for device id: {id}");
+            {
+                throw new HttpResponseException(new HttpResponseMessage(HttpStatusCode.InternalServerError)
+                {
+                    Content = new StringContent($"Failed to deseralized device twin tags for device id: {id}"),
+                    ReasonPhrase = "Critical Exception"
+                });
+            }
 
             // check the passed mac address matches what we have in the device twin.
             if (deviceTags.MacAddress != macAddress)
-                throw new Exception($"MacAddress in device twin does not match the MacAddress of {macAddress} passed");
+            {
+                throw new HttpResponseException(new HttpResponseMessage(HttpStatusCode.BadRequest)
+                {
+                    Content = new StringContent($"MacAddress in device twin does not match the MacAddress of {macAddress} passed"),
+                    ReasonPhrase = "Critical Exception"
+                });
+            }
 
+            // now generate SAS token.
 
+            var builder = IotHubConnectionStringBuilder.Create(iotHubConnectionString);
 
-            return id + macAddress;
+            int tokenExpiry = 8;
+            if (tokenExpiryInHours != 0)
+                tokenExpiry = tokenExpiryInHours;
+
+            SharedAccessSignatureBuilder sasBuilder;
+            if (signUsingPrimaryKey)
+            {
+                sasBuilder = new SharedAccessSignatureBuilder()
+                {
+                    Key = device.Authentication.SymmetricKey.PrimaryKey,
+                    Target = $"{builder.HostName}/devices/{WebUtility.UrlEncode(id)}",
+                    TimeToLive = TimeSpan.FromHours(Convert.ToDouble(tokenExpiry))
+                };
+            }
+            else
+            {
+                // then use secondary key.
+                sasBuilder = new SharedAccessSignatureBuilder()
+                {
+                    Key = device.Authentication.SymmetricKey.SecondaryKey,
+                    Target = $"{builder.HostName}/devices/{WebUtility.UrlEncode(id)}",
+                    TimeToLive = TimeSpan.FromHours(Convert.ToDouble(tokenExpiry))
+                };
+            }
+
+            return $"HostName={builder.HostName};DeviceId={id};SharedAccessSignature={sasBuilder.ToSignature()}";
         }
 
   
